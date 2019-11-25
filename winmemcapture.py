61cd9dbee11f1c3f439dcfd34114f768a3dfa6f5
Cyber Threat Hunter <threathunternotebook@gmail.com>
2019-11-25 15:03:47 -0500

    Create winmemcapture.py
    Example grr python hack to collect memory from Windows 10 workstation.  We use the ramcapture memory collection tool.  Include example of downloading, running memory capture tool and uploading results to a share.

---
+++ winmemcapture.py
@@ -0,0 +1,44 @@
+from subprocess import check_output
+import shutil
+import os
+share = r"\\192.168.229.75\grrshare"
+user = r"\doorman"
+pwd = "doorman"
+
+from contextlib import contextmanager
+
+@contextmanager
+def network_share_auth(share, user, pwd, drive_letter='P'):
+
+   cmd_parts = ["NET USE %s: %s" % (drive_letter, share)]
+   if user:
+      cmd_parts.append(" /USER:%s " % user)
+   if pwd:
+       cmd_parts.append(pwd)
+   os.system(" ".join(cmd_parts))
+   try:
+      yield
+   finally:
+      os.system("NET USE %s: /DELETE" % drive_letter)
+
+def get_memtool(bitnumber='64bit'):
+   if bitnumber == '64bit':
+      with network_share_auth(share, user, pwd):
+          shutil.copyfile(r"P:\x64\RamCapture64.exe", r"C:\RamCapture64.exe")
+          shutil.copyfile(r"P:\x64\RamCaptureDriver64.sys", r"C:\RamCaptureDriver64.sys")
+          shutil.copyfile(r"P:\x64\msvcp110.dll", r"C:\msvcp110.dll")
+          shutil.copyfile(r"P:\x64\msvcr110.dll", r"C:\msvcr110.dll")
+          shutil.copyfile(r"P:\x64\ramcapturedriver.cat", r"C:\ramcapturedriver.cat")
+
+def run_memdump():
+   output = check_output("c:\\RamCapture64.exe c:\\grr_memdump64.bin", shell=True)
+   magic_return_str = "memdump output %s" % output
+
+def upload_memfile():
+   with network_share_auth(share, user, pwd):
+      sharename = r"P:\grr_memdump64.bin"
+      shutil.copy(r"C:\grr_memdump64.bin", sharename)
+
+get_memtool()
+run_memdump()
+upload_memfile()
